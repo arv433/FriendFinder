ab318890c528545a00d58982625849016f5582db
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-09-23 14:31:59 -0400

    Added static routing

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,2 @@
+exports.surveyRoute = "/app/public/survey.html";
+exports.homeRoute = "/app/public/home.html";
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var http = require("http");
+var fs = require("fs");
+var routes = require("./app/routing/htmlRoutes")
+
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+    fs.readFile(__dirname + routes.surveyRoute, function(err, data) {
+        if (err) throw err;
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+    });
+};
+
+server.listen(PORT, function() {
+    console.log("Server is listening on PORT: " + PORT);
+});
