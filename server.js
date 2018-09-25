5bc682aac0a7b705438da936dd7a744eed21e31b
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-09-25 10:58:03 -0400

    Added express routing and modified hyperlinks on static pages

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
@@ -7,8 +5,16 @@ var http = require("http");
 var fs = require("fs");
 var routes = require("./app/routing/htmlRoutes");
 
+var app = express();
+
 var PORT = 8080;
 
+// manipulating path
+var pathObj = path.parse(__filename);
+console.log(process.env.PATH);
+console.log(process.env.PATH.split(path.delimiter));
+
+// server declaration, handling, and listen
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
@@ -17,7 +23,7 @@ function handleRequest(req, res) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(data);
     });
-};
+}
 
 server.listen(PORT, function() {
     console.log("Server is listening on PORT: " + PORT);
